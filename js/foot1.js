878245896fbe054e2932a153667941e2b922c79e
sgoblin <sgoblin@users.noreply.github.com>
2015-03-24 20:18:39 -0700

    Create foot1.js

---
+++ js/foot1.js
@@ -0,0 +1,4 @@
+function highlight(colorTo, elementTo) {
+				elementTo.style.background-color=colorTo;
+			}
+			document.getElementById("#footlink1").addEventListener("click", function(){highlight("deepskyblue", document.getElementById("foot1"));
